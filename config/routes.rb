46cff0e963ab69bd0400b20c46a2129337590fa4
Thanh-An Le <lethanhannicolas@gmail.com>
2020-02-25 11:28:21 +0100

    commit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :events do
+    resources :requests
+
+
+  resources :requests do
+    resources :messages, only: [:new, :create]
+  end
+  resources :messages, except: [:new, :create]
+
+  end
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
