99991c666b4b92e15ffee7f159e7bebc12387ed3
Jonas Pfuller <jonas.pfueller@outlook.com>
2020-02-26 18:14:31 +0100

    serach bar added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,8 +2,25 @@ class EventsController < ApplicationController
   before_action :find_event, only: [ :show, :destroy]
 
   def index
+    @genres = ["Disco", "Blues", "Funk", "Jazz", "Metal", "Pop", "Punk", "Rap", "Rock", "Country", "Reggae", "Gospel", "Soul", "Kompa", "Classic"]
+
     @events = policy_scope(Event)
+    if params[:genre].present?
+      @events = policy_scope(Event).where("genres ILIKE ?", "%#{params[:genre]}%")
+    end
+    if params[:instrument].present?
+      @events = policy_scope(Event).where("instruments ILIKE ?", "%#{params[:instrument]}%")
+    end
+    if params[:event_type].present?
+      @events = policy_scope(Event).where("event_types ILIKE ?", "%#{params[:event_type]}%")
+    end
+    if params[:arrondissement].present?
+      @events = policy_scope(Event).where("locations ILIKE ?", "%#{params[:arrondissement]}%")
+    end
+    return @events
   end
+
+  @artciles.where(name: params[:filter][:name])
 
   def show
     authorize @event
