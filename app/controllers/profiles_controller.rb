19f14386bd727f43819dea629647e93c4b899de7
vorachith phoubandith <songta17@hotmail.com>
2020-03-02 18:13:40 +0100

    add the show part 1

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,12 @@
 class ProfilesController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
-  #skip_after_action :verify_policy_scoped, only: :show
+  skip_after_action :verify_authorized, only: :show
 
   def index
     @users = User.all
   end
 
   def show
-    #@user = User.find(params[:id])
     @user = User.find(params[:id])
   end
 end
