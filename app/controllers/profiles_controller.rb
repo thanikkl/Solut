45bbea4b394be32556ad7af3c1099909be5b6d11
vorachith phoubandith <songta17@hotmail.com>
2020-03-02 17:57:08 +0100

    change on profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   skip_after_action :verify_policy_scoped, only: :index
+  #skip_after_action :verify_policy_scoped, only: :show
 
   def index
     @users = User.all
@@ -7,6 +8,6 @@ class ProfilesController < ApplicationController
 
   def show
     #@user = User.find(params[:id])
-    @user = authorize User.find(params[:id])
+    @user = User.find(params[:id])
   end
 end
