9d895067d652fbe16bc0402eed70660e4744b838
Jonas Pfuller <jonas.pfueller@outlook.com>
2020-03-04 17:49:12 +0100

    adj addresses

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,127 +39,8 @@ puts "Destroying all Instruments"
 Instrument.destroy_all
 puts "Instruments : destroyed"
 
-addresses = [
-  "16 Villa Gaudelet, Paris",
-  "2 Rue d'Alexandrie, Paris",
-  "11 Rue du Pot de Fer, Paris",
-  "23 Rue Clapeyron, Paris",
-  "4 Rue Lincoln, Paris",
-  "12 Rue Saint-Jean-Baptiste de la Salle, Paris",
-  "122 Boulevard Saint-Germain, Paris",
-  "180bis Rue de Charenton, Paris",
-  "Avenue Beaucour, 75008 Paris",
-  "Avenue Boutroux, 75013 Paris",
-  "Avenue Carnot, 75017 Paris",
-  "Avenue Cartellier, 75020 Paris",
-  "Avenue Charles-de-Foucauld, 75012 Paris",
-  "Avenue Charles-Floquet, 75007 Paris",
-  "Avenue Charles-Girault, 75008 Paris",
-  "Avenue Charles-Risler, 75007 Paris",
-  "Avenue Claude-Regaud, 75013 Paris",
-  "Avenue Claude-Vellefaux, 75010 Paris",
-  "Avenue Constant-Coquelin, 75007 Paris",
-  "Avenue Corentin-Cariou, 75019 Paris",
-  "Avenue Courteline, 75012 Paris",
-  "Avenue Daniel-Lesueur, 75007 Paris",
-  "Avenue Daumesnil, 75012 Paris",
-  "Avenue David-Weill, 75014 Paris",
-  "Avenue de Boufflers, 75116 Paris",
-  "Avenue de Bouvines, 75011 Paris",
-  "Avenue de Breteuil, 75007 Paris",
-  "Avenue de Champaubert, 75015 Paris",
-  "Avenue de Choisy, 75013 Paris",
-  "Avenue de Clichy, 75017 Paris",
-  "Avenue de Corbera, 75012 Paris",
-  "Avenue de Flandre, 75019 Paris",
-  "Avenue de Fontenay, 75012 Paris",
-  "Avenue de France, 75013 Paris",
-  "Avenue de Friedland, 75008 Paris",
-  "Avenue de Gravelle, 75012 Paris",
-  "Avenue de Joinville, 75012 Paris",
-  "Place Jules-Joffrin, 75018 Paris",
-  "Place Jules-Renard, 75017 Paris",
-  "Place Jules-Senard, 75019 Paris",
-  "Place Jussieu, 75005 Paris",
-  "Place Justin Godart, 75006 Paris",
-  "Place Kossuth, 75009 Paris",
-  "Place Lachambeaudie, 75012 Paris",
-  "Place Le Corbusier, 75006 Paris",
-  "Place Léon-Paul-Fargue, 75006 Paris",
-  "Place Léonard-Bernstein, 75012 Paris",
-  "Place Lili-Boulanger, 75009 Paris",
-  "Place Lino Ventura, 75009 Paris",
-  "Place Louis Bernier, 75017 Paris",
-  "Place Louis-Armand, 75015 Paris",
-  "Place Louis-Armstrong, 75013 Paris",
-  "Place Louis-Lépine, 75004 Paris",
-  "Place Louis-Marin, 75005 Paris",
-  "Place Lucien-Herr 75005 Paris",
-  "Place Madeleine Renaud et Jean Louis Barrault, 75015 Paris",
-  "Place Marc-Bloch, 75020 Paris",
-  "Place Marcel Cerdan, 75015 Paris",
-  "Place Marcel Paul, 75014 Paris",
-  "Place Marcel-Achard, 75019 Paris",
-  "Place Marcelin-Berthelot, 75005 Paris",
-  "Place Marguerite-de-Navarre, 75001 Paris",
-  "Place Maria-Callas, 75116 Paris",
-  "Place Marie-de-Miribel, 75020 Paris",
-  "Place Marie-Madeleine-Fourcade, 75015 Paris",
-  "Place Marlène Dietrich, 75116 Paris",
-  "Place Martin-Nadaud, 75020 Paris",
-  "Place Maubert, 75005 Paris",
-  "Place Maurice Audin, 75005 Paris",
-  "Place Maurice-Chevalier, 75020 Paris",
-  "Place Maurice-de-Fontenay, 75012 Paris",
-  "Place Maurice-Quentin, 75001 Paris",
-  "Place Mazas, 75012 Paris",
-  "Place Mehdi Ben Barka, 75006 Paris",
-  "Place Michel Petrucciani, 75018 Paris",
-  "Place Michel-Audiard, 75014 Paris",
-  "Rue Bouchardon, 75010 Paris",
-  "Rue Boucher, 75001 Paris",
-  "Rue Bouchut, 75015 Paris",
-  "Rue Boucry, 75018 Paris",
-  "Rue Boudreau, 75009 Paris",
-  "Rue Bougainville, 75007 Paris",
-  "Rue Bouilloux-Lafont, 75015 Paris",
-  "Rue Boulard, 75014 Paris",
-  "Rue Boulay, 75017 Paris",
-  "Rue Boulitte, 75014 Paris",
-  "Rue Boulle, 75011 Paris",
-  "Rue Bourdaloue, 75009 Paris",
-  "Rue Bouret, 75019 Paris",
-  "Rue Bourgon, 75013 Paris",
-  "Rue Boursault, 75017 Paris",
-  "Rue Bourseul, 75015 Paris",
-  "Rue Boussingault, 75013 Paris",
-  "Rue Boutarel, 75004 Paris",
-  "Rue Boutebrie, 75005 Paris",
-  "Rue Bouvier, 75011 Paris",
-  "Rue Boy-Zelenski, 75010 Paris",
-  "Rue Boyer, 75020 Paris",
-  "Rue Boyer-Barret, 75014 Paris",
-  "Rue Brahms, 75012 Paris",
-  "Rue Brancion, 75015 Paris",
-  "Rue Brémontier, 75017 Paris",
-  "Rue Bretonneau, 75020 Paris",
-  "Rue Brey, 75017 Paris",
-  "Rue Bridaine, 75017 Paris",
-  "Rue Brignole, 75116 Paris",
-  "Rue Brillat-Savarin, 75013 Paris",
-  "Rue Briquet, 75018 Paris",
-  "Rue Broca, 75005 Paris",
-  "Rue Brochant, 75017 Paris",
-  "Rue Brongniart, 75002 Paris",
-  "Rue Broussais, 75014 Paris",
-  "Rue Bruant, 75013 Paris",
-  "Rue Bruller, 75014 Paris",
-  "Rue Brunel, 75017 Paris",
-  "Rue Bruneseau, 75013 Paris",
-  "Rue Buffault, 75009 Paris",
-  "Rue Buffon, 75005 Paris",
-  "Rue Buot, 75013 Paris"
-]
+addresses = (75000..75020).to_a
+addresses = addresses.map! { |i| i.to_s }
 
 pseudo_array =
     [
@@ -171,7 +52,6 @@ pseudo_array =
     Faker::Music::RockBand.name.tr(" ", "_"),
     Faker::Music.band.tr(" ", "_")
     ]
-
 
 song_array = [
   "7 Things",
@@ -299,7 +179,7 @@ puts "Creating Events..."
 number_of_users.times do
   event_type = Event::TYPE_EVENT_ARRAY.sample
   genre_pref = Event::GENRE_ARRAY.sample
-  address = addresses.sample
+  address = addresses.sample + ", Paris"
   song = song_array.sample
   instruments = Event::EVENT_INSTRUMENTS_ARRAY.sample(rand(1..3))
   event = Event.new(
@@ -328,7 +208,6 @@ number_of_users.times do
     )
   user_media.save!
 end
-
 
 Event.all.each do |event|
   ContactRequest.create(
