d095456216e78ea4cab0d350a9b94f2916d99fb1
Jonas Pfuller <jonas.pfueller@outlook.com>
2020-03-05 10:57:23 +0100

    minor wording change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,17 +20,17 @@ Turbolinks.start()
 document.addEventListener('turbolinks:load', () => {
   initNavbarMenu();
   requestStatus();
-  initSweetalert();
+  // initSweetalert();
     // Do your stuff!
 })
 
-initSweetalert('#sweet-alert-demo', {
-  title: "Request",
-  text: "Request Sent",
-  icon: "success"
-}, (value) => {
-  console.log(value);
-});
+// initSweetalert('#sweet-alert-demo', {
+//   title: "Request",
+//   text: "Request Sent",
+//   icon: "success"
+// }, (value) => {
+//   console.log(value);
+// });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
