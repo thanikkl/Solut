a9bc74b3a8dcad6b2edf3eec4afc69f4fa292651
Thanh-An Le <lethanhannicolas@gmail.com>
2020-03-06 11:34:14 +0100

    setup

--- config/puma.rb
+++ config/puma.rb
@@ -26,11 +26,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-meta_product_name: "SOLUT"
-meta_title: "SOLUT - Bring musicians together"
-meta_description: "Platform to meet up with other musicians"
-meta_image: "app/assets/images/solut_logo_final.png" # should exist in `app/assets/images/`
-twitter_account: "@product_twitter_account"   # required for Twitter Cards
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
